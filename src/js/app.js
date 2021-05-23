e2988c579857e2435f1ecb192bd4032f5aa81db2
Roman Prilepsky <r616on@gmail.com>
2021-05-23 18:19:30 +0300

    init

---
+++ src/js/app.js
@@ -0,0 +1,16 @@
+import Team from './Team';
+import Bowerman from './person/Bowerman';
+import Swordsman from './person/Swordsman';
+import Magician from './person/Magician';
+import Undead from './person/classUndead';
+
+const bowerman = new Bowerman('Bower');
+const swordsman = new Swordsman('Sword');
+const magician = new Magician('Magic');
+const undead = new Undead('Unded');
+
+const team = new Team();
+team.addAll(bowerman, swordsman, magician, undead);
+for (const item of team) {
+  console.log(item);
+}
